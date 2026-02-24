f655564215636ef90d1e3ff38b1065ebf61b58e2
garima2508 <gg0509@srmist.edu.in>
2026-02-24 08:54:13 +0530

    UC4: Character Array Based Palindrome Check

--- PalindrmeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindrmeCheckerApp/src/PalindromeCheckerApp.java
@@ -2,19 +2,23 @@ import java.lang.*;
 import java.util.Scanner;
 public class PalindromeCheckerApp {
     /** program to reverse a string using loops */
-    public static void main(String [] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string to check if it is a palindrome:");
-        String originalString = scanner.nextLine();
-        String reversedString = "";
-        int length = originalString.length();
-        for (int i = length - 1; i >= 0; i--) {
-            reversedString = reversedString + originalString.charAt(i);
+    public static boolean isPalindrome(String inputString) {
+        char[] charArray = inputString.toCharArray();
+        int start = 0;
+        int end = charArray.length - 1;
+        while (start < end) {
+            if (charArray[start] != charArray[end]) {
+                return false;
+            }
+            start++;
+            end--;
         }
-        if (originalString.equals(reversedString)) {
-            System.out.println("Input string is a palindrome.");
-        } else {
-            System.out.println("Input string is not a palindrome.");
-        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String testString1 = "madam";
+        String testString2 = "hello";
+        System.out.println("\"" + testString1 + "\" is a palindrome: " + isPalindrome(testString1));
+        System.out.println("\"" + testString2 + "\" is a palindrome: " + isPalindrome(testString2));
     }
 }
